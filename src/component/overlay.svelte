0e618b51fbd8c3ff56a42e692558aeeb04978e9c
Axel669 <haryuu.hanekata@gmail.com>
2021-05-21 14:16:53 -0700

    starting on overlay items

---
+++ src/component/overlay.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {Portal} from "svelte-doric"
+
+    import GridCard from "./grid-card.svelte"
+</script>
+
+<style>
+    overlay-window {
+        position: fixed;
+    }
+</style>
+
+<Portal>
+    <overlay-window>
+        <slot />
+    </overlay-window>
+</Portal>
